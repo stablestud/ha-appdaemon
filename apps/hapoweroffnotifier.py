6a7f779f8518e0b29f88b369f0d6db36ff434f5a
Alexander Gottweis <stablestud@protonmail.com>
2024-08-30 14:54:32 +0200

    Improve notification on HA power outages

--- apps/hapoweroffnotifier.py
+++ apps/hapoweroffnotifier.py
@@ -3,11 +3,19 @@ import hassapi as ha
 class HAPowerOffNotifier(ha.Hass):
 
     def initialize(self):
+        off_states = ["off", "unavailable", "unknown"]
         self.set_namespace("homeassistant")
         self.switch = self.get_entity(self.args["entity_power"])
         self.notify_targets = self.args["notify_targets"]
-        self.switch.listen_state(self.trigger_poweroff, new="off")
+        self.switch.listen_state(self.trigger_poweroff, new=lambda s: s in off_states)
+        self.switch.listen_state(self.trigger_poweron, new=lambda s: s not in off_states)
+
+
+    def trigger_poweron(self, entity, attribute, old, new, cb_args):
+        for target in self.notify_targets:
+            self.call_service("notify/" + target, title="HomeAssistant", message=f"RECOVERY: Smarthome power is now back ONLINE ({new})")
+
 
     def trigger_poweroff(self, entity, attribute, old, new, cb_args):
         for target in self.notify_targets:
-            self.call_service("notify/" + target, title="HomeAssistant", message="Smarthome power is OFFLINE, running now on UPS")
+            self.call_service("notify/" + target, title="HomeAssistant", message=f"WARNING: Smarthome power went OFFLINE ({new}), running now on UPS" )
