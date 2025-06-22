e869ebab64019ddc4ad6a6c5197a13b0f0d8820b
Alexander Gottweis <stablestud@protonmail.com>
2025-06-22 07:01:21 +0200

    Add tagreader storeroom light automation

--- apps/storeroommotion.py
+++ apps/storeroommotion.py
@@ -2,11 +2,11 @@ import hassapi as ha
 import enum as enum
 
 class StoreroomMotion(ha.Hass):
-
     class TriggerMode(enum.Enum):
-        MANUAL = 1
-        MOTION = 2
-
+        MANUAL  = 1,
+        MOTION  = 2,
+        SERVICE = 3 # Manually turned ON via appdaemon service call
+                    # In this case the lights stays ON until its turned off manually
 
     def initialize(self):
         self.set_namespace("homeassistant")
@@ -14,7 +14,7 @@ class StoreroomMotion(ha.Hass):
         self.state = None
         self.initialize_args()
         self.initialize_listen_states()
-
+        self.initialize_service()
 
     def initialize_args(self):
         self.timer = self.get_entity(self.args["entity_timer"])
@@ -23,7 +23,6 @@ class StoreroomMotion(ha.Hass):
         self.motion_timeout = self.args["motion_timeout"]
         self.manual_timeout = self.args["manual_timeout"]
 
-
     def initialize_listen_states(self):
         self.sensor.listen_state(self.sensor_detected, new="on")
         self.sensor.listen_state(self.sensor_cleared, new="off")
@@ -31,6 +30,24 @@ class StoreroomMotion(ha.Hass):
         self.switch.listen_state(self.switch_off, new="off")
         self.listen_event(self.timer_finished, "timer.finished", entity_id = self.args["entity_timer"])
 
+    def initialize_service(self):
+        self.register_service("storeroom/light", cb=self.service_handler, namespace="default")
+
+    def service_handler(self, namespace, domain, service, kwargs):
+        assert "state" in kwargs.get("service_args")
+        self.ignore_action = False
+        state = kwargs.get("service_args")["state"]
+        if self.str2bool(state):
+            self.switch_on_service()
+        else:
+            self.switch_off(None, None, None, None, None)
+
+    def switch_on_service(self):
+        self.log("Switch via service call ON")
+        self.state = self.TriggerMode.SERVICE
+        if self.is_timer_active():
+            self.timer.call_service("cancel")
+        self.turn_on_light()
 
     def switch_on(self, entity, attribute, old, new, cb_args):
         if self.is_ignore_action():
@@ -42,7 +59,6 @@ class StoreroomMotion(ha.Hass):
         else:
             self.state = self.TriggerMode.MANUAL
             self.start_timer(self.manual_timeout)
-
     
     def switch_off(self, entity, attribute, old, new, cb_args):
         self.state = None
@@ -52,7 +68,6 @@ class StoreroomMotion(ha.Hass):
         if self.is_timer_active():
             self.timer.call_service("cancel")
 
-
     def sensor_detected(self, entity, attribute, old, new, cb_args):
         self.log("Motion detected")
         # If manually actived light
@@ -60,10 +75,11 @@ class StoreroomMotion(ha.Hass):
             self.log("Switching to motion based turn off")
             self.state == self.TriggerMode.MOTION
             self.timer.call_service("cancel")
+        elif self.state == self.TriggerMode.SERVICE:
+            return
         else:
             self.state = self.TriggerMode.MOTION
             self.turn_on_light()
-
 
     def sensor_cleared(self, entity, attribute, old, new, cb_args):
         # If not manually triggered
@@ -71,24 +87,19 @@ class StoreroomMotion(ha.Hass):
             self.log("Motion cleared")
             self.start_timer(self.motion_timeout)
 
-
     def timer_finished(self, event_name, data, cb_args):
         self.log("Timer 'light_timeout' finished")
         if self.is_light_on():
             self.turn_off_light()
 
-
     def is_light_on(self):
         return self.switch.get_state("state") == "on"
-
 
     def is_timer_active(self):
         return self.timer.get_state("state") == "active"
 
-
     def is_occupied(self):
         return self.sensor.get_state("state") == "on"
-
 
     def is_ignore_action(self):
         if self.ignore_action == True:
@@ -97,18 +108,15 @@ class StoreroomMotion(ha.Hass):
         else:
             return False
 
-
     def turn_on_light(self):
         self.log("Turning ON storeroom light")
         self.ignore_action = True
         self.switch.call_service("turn_on")
 
-
     def turn_off_light(self):
         self.log("Turning OFF storeroom light")
         self.ignore_action = True
         self.switch.call_service("turn_off")
-
 
     def start_timer(self, duration):
         if self.is_timer_active():
@@ -116,3 +124,12 @@ class StoreroomMotion(ha.Hass):
         else:
             self.log(f"Starting timer 'light timeout': duration: {duration}s")
         self.timer.call_service("start", duration = duration)
+
+    def str2bool(self, value):
+        if isinstance(value, str) or sys.version_info[0] < 3 and isinstance(value, basestring):
+            value = value.lower()
+            if value in {'yes', 'true', 'on', 't', 'y', '1'}:
+                return True
+            if value in {'no', 'false', 'off', 'f', 'n', '0'}:
+                return False
+        return None
--- apps/tagreader.py
+++ apps/tagreader.py
@@ -10,22 +10,33 @@ This allows custom actions to be taken on tag scans
 Configuration:
 - `tags`: list of tags that trigger actions
   - `tag_id`: home assistant tag id
-  - `action`: function inside TagReader class that should be called
-- `actions`: list of actions that are being triggered (optional; can be omitted if no additional data needs to be passed)
+  - `action`: function name inside TagReader class that should be called
+    - `name`: function name (optional)
+    - `action_args`: list of objects that should be passed to the action (optional)
+- `actions`: list of actions that are being triggered (optional; can be omitted if no additional action_args needs to be passed)
   - `name`: function name
-  - `data`: list of objects that should be passed to the action
+  - `action_args`: list of objects that should be passed to the action
 '''
 class TagReader(ha.Hass):
     def initialize(self):
         self.set_namespace("homeassistant")
+        action = None
         for tag in self.args["tags"]:
-            assert tag["action"] in TagReader.__dict__, f"action/function '{tag['action']}' not implemented in TagReader"
-            self.listen_event(callback=self.get_action_callback(tag["action"]), event="tag_scanned", tag_id=tag["tag_id"], data=self.get_action_data(tag["action"]))
+            if isinstance(tag["action"], str):
+                action = tag["action"]
 
-    def get_action_data(self, action_name):
+            elif isinstance(tag["action"], dict):
+                action = tag["action"]["name"]
+            assert action is not None, f"failed to read action name for tag '{tag['tag_id']}'"
+            assert action in TagReader.__dict__, f"action/function '{action}' not implemented in TagReader"
+            self.listen_event(callback=self.get_action_callback(action), event="tag_scanned", tag_id=tag["tag_id"], action_args=self.get_action_args(tag))
+
+    def get_action_args(self, tag):
+        if "action_args" in tag["action"]:
+            return tag["action"]["action_args"]
         for action in self.args["actions"]:
-            if action["name"] == action_name:
-                return action["data"] if "data" in action else []
+            if action["name"] == tag["action"]:
+                return action["action_args"] if "action_args" in action else []
         return []
 
     def get_action_callback(self, action):
@@ -34,19 +45,19 @@ class TagReader(ha.Hass):
         return callback
 
     def select_spool(self, event_name, event_data, kwargs):
-        data = kwargs.get("data")
-        self.reload_spools(data["spoolman_device"], data["spoolman_integration"])
+        action_args = kwargs.get("action_args")
+        self.reload_spools(action_args["spoolman_device"], action_args["spoolman_integration"])
         self.log(f"Tag read for {event_data['name']}")
         spool_id = 0
         spool_friendly_name = None
         if event_data["tag_id"] != "null_spool":
-            spool_entity = self.get_spool_entity(data["spoolman_device"], event_data["tag_id"])
+            spool_entity = self.get_spool_entity(action_args["spoolman_device"], event_data["tag_id"])
             if spool_entity is None:
                 self.log(f"Warning: no spool found for {event_data['name']}")
                 return
             spool_id = spool_entity.get_state(attribute="id")
             spool_friendly_name = spool_entity.get_state(attribute='friendly_name')
-        response = requests.post(data["moonraker_url"], json={"spool_id": spool_id})
+        response = requests.post(action_args["moonraker_url"], json={"spool_id": spool_id})
         if response.status_code == 200:
             self.log(f"Successfully set '{spool_friendly_name}' as active spool")
         else:
@@ -62,3 +73,15 @@ class TagReader(ha.Hass):
 
     def reload_spools(self, spoolman_device, spoolman_integration):
         self.call_service(service="homeassistant/reload_config_entry", device_id=spoolman_device, entry_id=spoolman_integration)
+
+    def execute_service(self, event_name, event_data, kwargs):
+        action_args = kwargs.get("action_args")
+        self.log(f"Tag read for {event_data['name']}")
+        service_args = {}
+        namespace = "default"
+        assert "service" in action_args
+        if "namespace" in action_args:
+            namespace = action_args["namespace"]
+        if "service_args" in action_args:
+            service_args = action_args["service_args"]
+        self.call_service(service=action_args["service"], namespace=action_args["namespace"], service_args=service_args)
